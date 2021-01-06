f71e8d855981b28305a24f1db7661d637e2cbe5a
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-06 23:34:36 +0530

    Create memory_access.v

---
+++ memory_access.v
@@ -0,0 +1,32 @@
+module memory_access(
+          input [31:0] op2,
+          input [31:0] aluresult,
+          input isld,
+          input isst,
+          output [31:0] ldresult
+          );
+          
+wire [31:0] op2,aluresult;
+wire isld,isst;
+reg [31:0] ldresult;
+
+
+           always @(*)
+           begin     
+           #2                                   // waiting for aluresult to come.
+           if (isld == 1)
+           begin
+           p.am1    = aluresult ;
+           ldresult = p.dm1;
+           p.rw     = 0;
+          end
+          else if ( isst == 1)
+          begin
+         
+           p.rw  = 1;
+           p.am2 = aluresult ;
+           p.dm3 = op2 ;
+          end
+          
+          end
+          endmodule 

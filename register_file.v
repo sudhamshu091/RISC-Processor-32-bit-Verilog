d6b4d8f6a3d90de98e204b223e42cfb596f1b580
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-07 00:45:01 +0530

    Create register_file.v

---
+++ register_file.v
@@ -0,0 +1,40 @@
+module register_file( input [3:0] a1, input [3:0] a2, input [3:0] a3, input [31:0] d3, input reset, input clk, input iswb, output [31:0] d1, output [31:0] d2 );
+
+wire [3:0] a1,a2,a3;
+wire [31:0] d3;
+wire reset,clk;
+reg [31:0] d1,d2;
+reg [31:0] r [15:0];
+reg [15:0]ind;
+wire iswb;
+
+always @(reset)
+                   begin
+if (reset==1)
+                   begin
+                   for (ind=0;ind<16;ind=ind+1)
+                   begin
+                   if (ind!=14)
+                   r[ind]  = 32'b0;
+                   else 
+                   r[ind]  = 32'h1000;
+                   end
+                   end
+                   end
+                   
+                     always @(clk or a1 or a2)
+                     begin
+                     if (reset!=1 && clk==0)                      // Getting operands from register file
+                     begin                     
+                     d1 = r[a1];
+                     d2 = r[a2];
+                     end
+                     end
+
+                     always @(posedge clk)
+                     begin                                       // Storing result in the register file
+                     if (reset!=1 && iswb==1)
+                     r[a3] <= d3;
+                     end
+
+endmodule
